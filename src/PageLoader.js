4abc326a472466a61a1a2e28258822b1cbb4f577
coffeeandcandle <145921885+coffeeandcandle@users.noreply.github.com>
2023-10-03 17:22:12 +0100

    default page loader

--- src/PageLoader.js
+++ src/PageLoader.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import NavBar from './layouts/NavBar';
 import Home from './pages/home';
 import Work from './pages/work';
@@ -10,33 +10,35 @@ import GetInTouch from './pages/getintouch';
 import PrivacyPolicy from './pages/privacypolicy';
 import { data } from './data/data';
 
-function PageLoader() {
-  const routes = data.map((page) => {
-    return (
-      <Route
-        key={page.id}
-        path={`/${page.linkName}`}
-        element={<DynamicPage pageInfo={page} />}
-      />
-    );
-  });
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavBar} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/test" element={<DynamicPage />} />
-        <Route path="/agency" element={<Agency />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/get-in-touch" element={<GetInTouch />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        {routes}
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+function PageLoader() {
+
+    const routes = data.map((page) => {
+        return <Route key={page.id} path={`/${page.linkName}`} 
+            element={<DynamicPage pageInfo={page}/>} />
+    })
+
+    
+
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<NavBar />} >
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/work' element={<Work />} />
+                    <Route path='/test' element={<DynamicPage />} />
+                    <Route path='/agency' element={<Agency />} />
+                    <Route path='/contact' element={<Contact />} />
+                    <Route path='/get-in-touch' element={<GetInTouch />} />
+                    <Route path='/privacypolicy' element={<PrivacyPolicy />} />
+                    {routes}
+                    <Route path="*" element={<Navigate to="/home" replace />} />
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default PageLoader;
+export default PageLoader
