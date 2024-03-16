2c13f86d16da95e1d8ef74cd2981780b31382530
coffeeandcandle <145921885+coffeeandcandle@users.noreply.github.com>
2024-03-16 11:49:23 +0000

    Addition of Data set 9

--- src/data/data.js
+++ src/data/data.js
@@ -159,4 +159,24 @@ export const data = [
             { right: [require('../Place Holder Images/GLADE_OF_LIGHT_HR-47_EDIT.jpeg'), require('../Place Holder Images/GLADE_OF_LIGHT_HR-52.jpeg'), require('../Place Holder Images/GLADE_OF_LIGHT_HR-65_EDIT.jpeg'), require('../Place Holder Images/GoL_SW_Site-03.png')] }
         ]
     },
+    {
+        id: 9,
+        linkName: 'Gate of Yemen',
+        projectName: 'Gate of Yemen',
+        headerImage: require('../Place Holder Images/Manchester_1.jpeg'),
+        textBox: [{ Client: 'The Manchester Memorial Trust' }, { Partnership: 'BCA Landscape' }, { 'What We Did': 'Lorem Ipsum; Lorem Ipsum; Lorem Ipsum; Lorem Ipsum' }, {
+            Awards: `ICE NW Civil Engineering Special Recognition Award;
+        Longlisted – Dezeen Awards 2022;
+        International Loop Design – People’s Choice Awards`}, { Photography: 'Matt Bell' }],
+        description: `The Glade of Light – a place of tranquillity and a celebration of life. A collaboration with BCA Landscape.
+            It was an honour to be chosen to help the bereaved families and the wider team imagine and realise this memorial garden. The design ideas and thoughts behind ‘The Glade of Light’ have always come from a place of heart-felt respect and deep sympathy. Central to the space is a 
+            floating marble halo, inset in bronze with the names of the 22 concertgoers that lost their lives in the Manchester Arena attack in 2017. 
+            We believe design is about and for people and it is not a fixed thing on a plan, but consists of an ever-changing, living and shifting series of moments.
+            Our wish for this garden is that it will allow everyone to linger in these moments and search for stillness.`,
+        images: [
+            { mid: [require('../Place Holder Images/GoL_SW_Site-05.png'), require('../Place Holder Images/GoL_SW_Site-06.png'), require('../Place Holder Images/Manchester_1.jpeg'), require('../Place Holder Images/Screenshot+2023-03-17+at+16.45.58.png')] },
+            { left: [require('../Place Holder Images/02-A4-Magazine-Mockups-v2b.jpeg'), require('../Place Holder Images/02.jpeg'), require('../Place Holder Images/FORMAT_B2_BOOK_06+(1).jpeg'), require('../Place Holder Images/GLADE_OF_LIGHT_HR-8.jpeg')] },
+            { right: [require('../Place Holder Images/GLADE_OF_LIGHT_HR-47_EDIT.jpeg'), require('../Place Holder Images/GLADE_OF_LIGHT_HR-52.jpeg'), require('../Place Holder Images/GLADE_OF_LIGHT_HR-65_EDIT.jpeg'), require('../Place Holder Images/GoL_SW_Site-03.png')] }
+        ]
+    },
 ]
