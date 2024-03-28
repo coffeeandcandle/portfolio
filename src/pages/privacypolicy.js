28829d328b493d48b3ff58b47fa970476c24beb9
coffeeandcandle <145921885+coffeeandcandle@users.noreply.github.com>
2024-03-28 17:33:25 +0000

    Policy Colour and title

--- src/pages/privacypolicy.js
+++ src/pages/privacypolicy.js
@@ -7,20 +7,20 @@ function PrivacyPolicy() {
             <div className='panel fitscreenSec bannerSec blueBg'>
                 <div className='container'>
 
-                    <h1 className='panel contactTitle fadeInUp'>Privacy Policy for Coffee & Candle </h1>
+                    <h3 className='panel contactTitle fadeInUp'>Privacy Policy</h3>
                     <section className='simpleContent' style={{ backgroundImage: 'none' }} >
                         <div className='simpleContentInner'>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>At Coffee & Candle Design Agency, we are committed to protecting the privacy of our users. <br />
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>At Coffee & Candle Design Agency, we are committed to protecting the privacy of our users. <br />
                                 This privacy policy outlines the types of personal information we collect, how we use it, and how we protect it.
                             </p>
-                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'white', fontSize: '32px' }}>Information we collect</h1>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>When you visit our website, we may collect certain information about you, including your IP address,
+                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'black', fontSize: '32px' }}>Information we collect</h1>
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>When you visit our website, we may collect certain information about you, including your IP address,
                                 browser type, operating system, and the pages you visit. We may also collect personal
                                 information that you provide to us, such as your name, email address, and phone number,
                                 if you contact us via our website's contact form.
                             </p>
-                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'white', fontSize: '32px' }}>How We Use Your Information</h1>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>We use the information we collect to provide you with a better user experience and to communicate with you about our services. Specifically, we may use your information to:
+                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'black', fontSize: '32px' }}>How We Use Your Information</h1>
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>We use the information we collect to provide you with a better user experience and to communicate with you about our services. Specifically, we may use your information to:
                                 <br /><br />
                                 <ul style={{ textAlign: 'left' }}>
                                     <li>Respond to your inquiries</li>
@@ -31,23 +31,23 @@ function PrivacyPolicy() {
                                 <br />
                                 We do not sell or rent your personal information to third parties. However, we may share your information with our trusted third-party service providers who assist us in providing our services.
                             </p>
-                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'white', fontSize: '32px' }}>How We Protect Your Information</h1>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>We take the security of your personal information seriously and have implemented
+                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'black', fontSize: '32px' }}>How We Protect Your Information</h1>
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>We take the security of your personal information seriously and have implemented
                                 appropriate technical and organisational measures to safeguard it.
                                 We use industry-standard encryption technologies to protect your information during transmission and storage.
                             </p>
-                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'white', fontSize: '32px' }}>Links to Other Websites</h1>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>Our website may contain links to other websites that are not
+                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'black', fontSize: '32px' }}>Links to Other Websites</h1>
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>Our website may contain links to other websites that are not
                                 operated by us. We are not responsible for the privacy practices or content of these third-party websites.
                                 We encourage you to read the privacy policies of these websites before providing them with any personal information.
                             </p>
-                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'white', fontSize: '32px' }}>Changes to Our Privacy Policy</h1>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>We may update our privacy policy from time to time.
+                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'black', fontSize: '32px' }}>Changes to Our Privacy Policy</h1>
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>We may update our privacy policy from time to time.
                                 Any changes we make will be posted on this page,
                                 and we will notify you of any significant changes via email.
                             </p>
-                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'white', fontSize: '32px' }}>Contact Us</h1>
-                            <p className='panel fadeInUp' style={{ color: 'white', fontSize: '24px' }}>If you have any questions or concerns about our privacy policy or the way we handle
+                            <h1 className='panel fadeInUp contactTitle' style={{ color: 'black', fontSize: '32px' }}>Contact Us</h1>
+                            <p className='panel fadeInUp' style={{ color: 'black', fontSize: '24px' }}>If you have any questions or concerns about our privacy policy or the way we handle
                                 your personal information, please contact us via our website's contact form.
                             </p>
                             <br /><br /><br /><br />
